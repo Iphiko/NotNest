fcb31de2a5634c9a903cdcfd40031a54ece3937d
Iphiko <gromykom2010@mail.ru>
2025-08-03 17:44:30 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,47 @@
+let btn =  document.querySelector('button');
+let notesDiv = document.querySelector('.notes_container');
+let notes = document.querySelectorAll('input_text');
+
+function showNotes(){
+    notesDiv.innerHTML = localStorage.getItem('notes');
+}
+
+showNotes();
+
+function updateStorage(){
+    localStorage.setItem('notes', notesDiv.innerHTML);
+}
+
+btn.onclick = function(){
+    let inputBox = document.createElement('p');
+    let icon = document.createElement('i');
+    let container = document.createElement('div');
+    inputBox.className = 'input_text';
+    inputBox.setAttribute('contenteditable', 'true');
+    icon.className = 'bx bx-trash';
+    container.appendChild(inputBox);
+    container.appendChild(icon);
+    notesDiv.appendChild(container);
+}
+
+notesDiv.addEventListener('click', (e) => {
+
+    if (e.target.tagName === 'I') {
+        e.target.parentElement.remove();
+        updateStorage();
+    } else if (e.target.tagName === 'p') {
+        notes = document.querySelectorAll('.input_text');
+        notes.forEach(e =>{
+            e.onkeyup = function() {
+                updateStorage();
+            }
+        })
+    }
+})
+
+document.addEventListener('keydown', e =>{
+    if(e.key === 'enter'){
+        document.execCommand('InsertLineBreak');
+        e.preventDefault();
+    }
+})
